f181ec1e8bf160b46ce31fda4bd2180e690c8518
Busnax <ale8103@outlook.it>
2025-02-14 05:00:44 +0100

    test

--- AGAProject/src/it/unipv/ingsfw/aga/Test.java
+++ AGAProject/src/it/unipv/ingsfw/aga/Test.java
@@ -1,13 +1,25 @@
 package it.unipv.ingsfw.aga;
 
-import it.unipv.ingsfw.aga.database.InvitatoDAO;
-import it.unipv.ingsfw.aga.model.persone.Invitato;
+import it.unipv.ingsfw.aga.exceptions.MaxExeededException;
+import it.unipv.ingsfw.aga.model.biglietto.Biglietto;
+import it.unipv.ingsfw.aga.model.evento.Evento;
+import it.unipv.ingsfw.aga.model.persone.Persona;
+import it.unipv.ingsfw.aga.model.persone.PersonaFactory;
 
+import java.sql.Date;
 import java.sql.SQLException;
 
 public class Test {
-    public static void main(String[] args) throws SQLException {
-        Invitato nuovoInvitato = new Invitato("Mario", "Rossi", "test@email.com");
-        InvitatoDAO.aggiungiInvitato(nuovoInvitato);
+    public static void main(String[] args) throws SQLException, MaxExeededException {
+        Persona persona = PersonaFactory.creaPersona("Organizzatore", "Mario", "Rossi","test@gmail.com");
+        System.out.println(persona.toString());
+        Evento evento = persona.creaEvento("Concerto", new Date(2021, 10, 10), "Milano", 100);
+        System.out.println(evento.toString());
+        Biglietto b1 = evento.aggiungiBiglietto("Giovanni", "Bianchi", "gb@gmail,com", evento.getIdEvento(), false);
+        Biglietto b2 = evento.aggiungiBiglietto("Giuseppe", "Verdi", "gv@gmail,com", evento.getIdEvento(), false);
+        Biglietto b3 = evento.aggiungiBiglietto("Giuseppe", "Rossi", "gv@gmail,com", evento.getIdEvento(), true);
+        System.out.println(b1.toString());
+        System.out.println(b2.toString());
+        System.out.println(b3.toString());
     }
 }
