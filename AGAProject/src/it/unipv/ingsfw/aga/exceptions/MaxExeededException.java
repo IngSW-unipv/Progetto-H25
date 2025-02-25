5c3c5a40f9f21627ebd241aa0cef2ea238453e6f
Busnax <ale8103@outlook.it>
2025-02-25 23:06:10 +0100

    javadoc

--- AGAProject/src/it/unipv/ingsfw/aga/exceptions/MaxExeededException.java
+++ AGAProject/src/it/unipv/ingsfw/aga/exceptions/MaxExeededException.java
@@ -1,5 +1,7 @@
 package it.unipv.ingsfw.aga.exceptions;
-
+/**
+ * Eccezione lanciata quando il numero massimo di partecipanti è stato superato
+ */
 public class MaxExeededException extends Exception {
     public MaxExeededException() {
         super("Il numero massimo di partecipanti è stato superato");
